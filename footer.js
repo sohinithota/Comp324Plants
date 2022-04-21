e97de8ab797730487b2ea599e0807a715d73fd32
Youa-Yang <94993902+Youa-Yang@users.noreply.github.com>
2022-04-21 15:52:30 -0500

    Add files via upload

--- footer.js
+++ footer.js
@@ -1,8 +1,9 @@
 const createFooter = () => {
-let footer= document.querySelector('.centertable')
+let bottom= document.querySelector('.bottom')
 
-centertable.innerHTML =`
-  <table class = centertable >
+bottom.innerHTML =`
+  <div class = "bottom">
+    <table class = centertable >
       <tr>
         <th style = "color:green">About</th>
         <th style = "color:green">Services</th>
@@ -21,10 +22,13 @@ centertable.innerHTML =`
         <td><a href="Footer/Register.html">User Register</a></td>
       </tr>
 
+
+
       <tr>
         <td><a href="index.html">Home</a></td>
         <td><a href ="Footer/compliant.html">Open Dispute</a></td>
         <td><a href>My Carts</a></td>
+
       </tr>
 
     </table>
@@ -32,12 +36,11 @@ centertable.innerHTML =`
       <h6 class="title">Our app</h6> <a href="#"  data-abc="true"><img class="img-responsive" src="https://i.imgur.com/nkZP7fe.png" height="40"></a>
       <a href="#"  data-abc="true"><img class="img-responsive" src="https://i.imgur.com/47q2YGt.png" height="40" width="123"></a>
     </aside>
-
-    </section>
     <section class = centertable>
       <p> © 2022 Plantstill All rights reserved </p>
       <p target="_blank" > <a href="#" data-abc="true">Privacy &amp; Cookies</a> &nbsp; &nbsp; <a href="#" data-abc="true">Accessibility</a> </p>
     </section>
+  </div>
     `;
   }
 
