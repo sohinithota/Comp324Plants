3429f976c49c4af5926f2fee9f29a4aa24248218
Sohini Thota <sthota@luc.edu>
2022-04-21 17:27:06 -0500

    updated nav

--- nav.js
+++ nav.js
@@ -16,10 +16,10 @@ topnav.innerHTML =`
     </div>
     <div class="center">
       <ul id = "menutabs">
-         <li><a href="../navigation/house-plants0.html">house plants</a></li>
-         <li><a href="../navigation/succulents0.html">succulents</a></li>
-         <li><a href="../navigation/planters.html" target="_blank">planters</a></li>
-         <li><a href="../navigation/plant-accessories.html" target="_blank">plant accessories</a></li>
+         <li><a href="navigation/house-plants0.html">house plants</a></li>
+         <li><a href="navigation/succulents0.html">succulents</a></li>
+         <li><a href="navigation/planters.html" target="_blank">planters</a></li>
+         <li><a href="navigation/plant-accessories.html" target="_blank">plant accessories</a></li>
        </ul>
     </div>
     </div>
