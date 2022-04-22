21301d24d2f8fdf89b8769bf5bbf5098b9b6132a
Youa-Yang <94993902+Youa-Yang@users.noreply.github.com>
2022-04-21 20:54:49 -0500

    Add files via upload

--- nav.js
+++ nav.js
@@ -1,19 +1,20 @@
-
 const createTopNav = () => {
 let topnav= document.querySelector('.topnav')
 
 
 topnav.innerHTML =`
     <div class = "nav">
-    <div class="nav-items">
-      <div class="search">
-      <input type="text" class="search-box" placeholder="search product">
-      <button class="search-btn">search</button>
-      <a href="index.html">Home</a>
-      <a href="Login.html"><i class="fa-solid fa-user"></i></a>
-      <a href="wishlist.html"><i class="fa-solid fa-list"></i></a>
+      <div class="center">
+        <div class="nav-items">
+          <div class="search">
+          <input type="text" class="search-box" placeholder="search product">
+          <button class="search-btn">search</button>
+          <a href="index.html">Home</a>
+          <a href="Login.html"><i class="fa-solid fa-user"></i></a>
+          <a href="wishlist.html"><i class="fa-solid fa-list"></i></a>
+          </div>
+        </div>
       </div>
-    </div>
     <div class="center">
       <ul id = "menutabs">
          <li><a href="navigation/house-plants.html">house plants</a></li>
