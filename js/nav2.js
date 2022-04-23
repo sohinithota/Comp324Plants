c2e01e7f09b48b41231529e5a63bb04aa4c144ab
michal Galvan <mgalvan2@luc.edu>
2022-04-23 17:36:01 -0500

    finishing pathways

--- js/nav2.js
+++ js/nav2.js
@@ -8,18 +8,18 @@ topnav.innerHTML =`
           <div class="search">
           <input type="text" class="search-box" placeholder="search product">
           <button class="search-btn">search</button>
-          <a href="index.html">Home</a>
-          <a href="Footer/Login.html"><i class="fa-solid fa-user"></i></a>
+          <a href=../index.html">Home</a>
+          <a href="../Footer/Login.html"><i class="fa-solid fa-user"></i></a>
           <a href="wishlist.html"><i class="fa-solid fa-list"></i></a>
           </div>
         </div>
       </div>
     <div class="center">
       <ul id = "menutabs">
-         <li><a href="house-plants.html">house plants</a></li>
-         <li><a href="succulents.html">succulents</a></li>
-         <li><a href="planters.html">planters</a></li>
-         <li><a href="plant-accessories.html">plant accessories</a></li>
+         <li><a href="../navigation/house-plants.html">house plants</a></li>
+         <li><a href="../navigation/succulents.html">succulents</a></li>
+         <li><a href="../navigation/planters.html">planters</a></li>
+         <li><a href="../navigation/plant-accessories.html">plant accessories</a></li>
        </ul>
     </div>
     </div>
