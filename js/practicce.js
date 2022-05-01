226af54d3d66e9bfd888318d9a288c53e41b35ae
michael Galvan <mgalvan2@luc.edu>
2022-05-01 18:36:48 -0500

    More firebase functions

--- js/practicce.js
+++ js/practicce.js
@@ -1,8 +1,10 @@
-import {createFirebase, LoginAuth} from "../js/auth.js";
+import {createFirebase, getData, LoginAuth} from "../js/auth.js";
 import {Auth} from "../js/auth.js";
 
 export function myfunction() {
     const name = document.getElementById("fname1").value;
+    alert("Document written with ID: ");
+
     createFirebase();
     //location.replace("../footer/AboutUs.html");
 
@@ -10,13 +12,19 @@ export function myfunction() {
 }
 export function createAuth()
 {
-    //location.replace("../footer/AboutUs.html");
+    location.replace("../footer/AboutUs.html");
+
     LoginAuth();
 
 }
 
 export function LoginCheck()
 {
+
     Auth();
 }
 
+export function LoginData()
+{
+    getData()
+}
